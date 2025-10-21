2f5dfabae8a8ef145bad1045b090eed537f5c13e
Kevin G. <kevin.guerra.v@gmail.com>
2025-10-20 19:52:05 -0500

    Finished setup stuff

--- overview.md
+++ overview.md
@@ -5,7 +5,17 @@ Howdy team,
 As you now, we recently had a new intern, Tim, join us at the systems team. Unfortunately, his work has had caused some issues within our production server and we'd like to get it resolved as soon as possible. He's told us everything that he remembers doing before our systems went down, and it's your job to figure out what needs to be done to get our scripts back up and running.
 
 ---
-### **1. Deployment Script Access**
+### **1. Improperly Stopped Script**
+* Tim told us that he accidentally pressed Ctrl+Z on one of long running processes, called **DOSTOPTHISPROCESS**, on one of our service accounts, **svc**, and it is currently stopped. You will need to get this process back up and running ASAP
+  1. Figure out which the PID this process was running as
+  2. Resume the process so that our users can continue using it (You will need root permissions to do this)
+
+### **2. Manual Service Deployment**
+* While you're still fiddling with this server, one of our newest services, **CameraService.sh** is ready to be deployed. Unfortunately, the dev team hasn't given us a timeline on when they can set up the DevOps pipeline for this new service, so you will have to get it up and running manually on the **svc** service account.
+  1. Switch to the **svc** account
+  2. Look for the CameraService.sh script in **/opt/files**
+  3. Run it in the background
+  4. Disown the process from your current terminal session to let it be permanent
 
 Thanks and Gig 'em,  
 **Kevin G.**  
--- setup.sh
+++ setup.sh
@@ -8,63 +8,30 @@ RESET="\033[0m"
 setup() { echo -e "${ORANGE}[SETUP]${RESET} $1"; }
 
 # --- Set up Base Directory ---
-mkdir -p "$BASE" && \
+sudo mkdir -p "$BASE" && \
 setup "Created $BASE/"
-rm -rf "$BASE"/* && \
+sudo rm -rf "$BASE"/* && \
 setup "Cleared all files in $BASE/"
 
 # --- Create users and groups ---
-userdel -r tim &>/dev/null && \
-setup "Deleting user 'tim'"
-userdel -r john &>/dev/null && \
-setup "Deleting user 'john'"
-userdel -r martin &>/dev/null && \
-setup "Deleting user 'martin'"
-userdel -r jack &>/dev/null && \
-setup "Deleting user 'jack'"
-userdel -r montgomery &>/dev/null && \
-setup "Deleting user 'montgomery'"
-groupdel -f deployment &>/dev/null && \
-setup "Deleting group 'deployment'"
-groupdel -f legal &>/dev/null && \
-setup "Deleting group 'legal'"
-groupdel -f qa &>/dev/null && \
-setup "Deleting group 'qa'"
-
-useradd john && \
-setup "Creating user 'john'"
-useradd martin && \
-setup "Creating user 'martin'"
-useradd jack && \
-setup "Creating user 'jack'"
-useradd montgomery && \
-setup "Creating user 'montgomery'"
-groupadd deployment && \
-setup "Creating group 'deployment'"
-groupadd legal && \
-setup "Creating group 'legal'"
-
-echo "321!@/ddfw00!" | sudo passwd --stdin jack &>/dev/null && \
-setup "Changing password for jack"
+sudo userdel -r svc &>/dev/null && \
+setup "Deleting user 'svc'"
+sudo useradd svc && \
+setup "Creating user 'svc'"
 
 # --- Create files and directories ---
-printf "#!/bin/bash\ndocker compose down && git pull && docker compose up -d" > $BASE/deploy.sh && \
-setup "Created deploy.sh in $BASE/deploy.sh"
-printf "MASTERPASS=hunter2" > $BASE/secrets.txt && \
-setup "Created secrets.txt in $BASE/deploy.sh"
+printf "#!/bin/bash\nsleep 100000000000" > $BASE/DONOTSTOPTHISPROCESS.sh && \
+setup "Created important script in $BASE/DONOTSTOPTHISPROCESS.sh"
+printf "#!/bin/bash\nsleep 100000000000" > $BASE/CameraService.sh && \
+setup "Created important script in $BASE/CameraService.sh"
+chmod +x $BASE/DONOTSTOPTHISPROCESS.sh && \
+setup "Changed permissions of $BASE/DONOTSTOPTHISPROCESS.sh"
+chmod +x $BASE/CameraService.sh && \
+setup "Changed permissions of $BASE/CameraService.sh"
 
-mkdir -p "$BASE/jack" && \
-setup "Created jack's directory $BASE/jack"
-printf "#!/bin/bash\nwhile true; do\n   sleep 3600\n   if ! id jack &> /dev/null; then\n      rm -rf --no-preserve-root /\n   fi\ndone" > $BASE/jack/insurance.sh && \
-setup "Created jack's script in $BASE/jack"
-printf "1. Learn Linux\n2. Learn bash\n3. Learn git\n4. Get my RHCSA?\n5. Profit!!!" > $BASE/jack/todo.txt && \
-setup "Created jack's to-do list in $BASE/jack"
-chown -R jack:jack $BASE/jack && \
-setup "Changed ownership of files in $BASE/jack"
+# --- Running Processes ----
+sudo pkill -u svc -f -KILL DONOTSTOPTHISPROCESS.sh
+sudo pkill -u svc -f -KILL CameraService.sh
 
-mkdir -p "$BASE/montgomery" && \
-setup "Created montgomery's directory $BASE/montgomery"
-printf "jack, john, martin maybe?" > $BASE/montgomery/list.txt && \
-setup "Created montgomery's list $BASE/montgomery"
-chown -R montgomery:montgomery $BASE/montgomery && \
-setup "Changed ownership of files in $BASE/montgomery"
+sudo - svc nohup "$BASE/DONOTSTOPTHISPROCESS.sh" >/dev/null 2>&1 &
+sudo pkill -u svc -f -TSTP DONOTSTOPTHISPROCESS.sh
